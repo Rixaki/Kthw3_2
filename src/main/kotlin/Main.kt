2d66e9d19580e1f4a84c51c0c30b217ec6bc9b91
Rinat <hakimovrinat@gmail.com>
2023-07-09 00:01:14 +0700

    Main.kt correcting

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,37 @@
+import kotlin.math.min
+
 fun main(args: Array<String>) {
     var amount: Int = 100
-    var cartType: String = "Maestro"
-    println(fee(cartType,500,amount))
+    var cartType: String = "Mir"
+    var fee: Int = fee(cartType, 0, amount)
+    println(if (fee != -1) fee else "Превышен лимит или недоступная платжная система.")
 }
 
-fun fee (cartType: String = "VKPay",
-          amountCurMonth: Int = 0,
-          amountThis: Int) : Int = when {
-              (cartType == "VKPay") -> 0
-                (cartType == "Visa") || (cartType == "Mir") -> (amountThis*0.01*0.75 + 35).toInt()
+fun fee(
+    cartType: String = "VKPay",
+    amountCurMonth: Int = 0,
+    amountThis: Int = 0
+): Int = when {
+    (cartType == "VKPay") && isInLimit("VKPay", amountCurMonth, amountThis) -> 0
 
-                ((cartType == "MasterCard") || (cartType == "Maestro")) &&
-                (amountCurMonth in 300..75_000) &&
-                (cartType != "VKPay") -> 0
+    (cartType == "Visa") || (cartType == "Mir") &&
+            isInLimit("", amountCurMonth, amountThis) -> min((amountThis * 0.01 * 0.75).toInt(), 35)
 
-              (cartType == "MasterCard") || (cartType == "Maestro") -> (amountThis*0.01*0.6 + 20).toInt()
-                else -> 0
-          }
+    ((cartType == "MasterCard") || (cartType == "Maestro")) &&
+            (amountCurMonth + amountThis in 300..75_000) &&
+            isInLimit("", amountCurMonth, amountThis) -> 0
+
+    (cartType == "MasterCard") || (cartType == "Maestro") &&
+            isInLimit("", amountCurMonth, amountThis) -> (amountThis * 0.01 * 0.6 + 20).toInt()
+
+    else -> -1
+}
+
+fun isInLimit(
+    cartType: String = "VKPay",
+    amountCurMonth: Int = 0,
+    amountThis: Int = 0
+): Boolean = when {
+    (cartType == "VKPay") -> (amountThis <= 15_000) && (amountCurMonth + amountThis <= 40_000)
+    else -> (amountThis <= 150_000) && (amountCurMonth + amountThis <= 600_000)
+}
